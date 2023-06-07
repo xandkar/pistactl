4242c0da0868a7d880e320f479c609a978457920
Siraaj Khandkar <siraaj@khandkar.net>
2023-06-07 18:13:10 -0400

    Enable overriding configs via CLI

--- src/lib/cli.rs
+++ src/lib/cli.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use expanduser::expanduser;
 
@@ -9,6 +11,22 @@ pub struct Cli {
     #[clap(short, long, default_value = concat!("~/.", crate::NAME!(), ".toml"))]
     config: String,
 
+    /// Increase logging verbosity
+    #[clap(short, long, default_value_t = false)]
+    debug: bool,
+
+    /// Name of tmux sock. Corresponds to tmux -L
+    #[clap(short = 'L', long)]
+    sock_name: Option<String>,
+
+    /// Name of tmux session
+    #[clap(short, long)]
+    session: Option<String>,
+
+    /// Directory where slot subdirectories with FIFOs will be created
+    #[clap(long)]
+    dir: Option<PathBuf>,
+
     #[clap(subcommand)]
     pub command: Cmd,
 }
@@ -16,7 +34,20 @@ pub struct Cli {
 impl Cli {
     pub fn to_cfg(&self) -> Result<Cfg> {
         let cfg_file = expanduser(&self.config)?;
-        Cfg::from_file(&cfg_file)
+        let mut cfg = Cfg::from_file(&cfg_file)?;
+        if self.debug {
+            cfg.debug = true;
+        }
+        if let Some(ref sock) = self.sock_name {
+            cfg.sock = sock.clone();
+        }
+        if let Some(ref session) = self.session {
+            cfg.session = session.clone();
+        }
+        if let Some(ref dir) = self.dir {
+            cfg.slots_fifos_dir = dir.clone();
+        }
+        Ok(cfg)
     }
 }
 
